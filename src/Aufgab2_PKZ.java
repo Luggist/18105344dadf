1b6fc5b914ca340381c0ee3f2f2bf7abbf7c890e
Stuefer Lukas <1810653445@stud.fh-kufstein.ac.at>
2018-11-08 11:38:51 +0100

    Aufgabe1

--- src/Aufgab2_PKZ.java
+++ src/Aufgab2_PKZ.java
@@ -1,2 +1,16 @@
 public class Aufgab2_PKZ {
+    public static void (String [] args){
+        String name1 = "Beispiel";//Zeichenkette
+        int Zahl1 = 22; //-2.147.483.648 ... 2.147.483.647
+        double Zahl2 = 22.25;// 	+/-4,9E-324 ... +/-1,7E+308
+        boolean Hunger = true; //true / false
+        long Zahl3 = 1818431;//-263 bis 263-1, ab Java 8 auch 0 bis 264 -1
+        char Zeichen1 = 'a'; //0 ... 65.535 (z. B. 'A')
+        byte byte1 = 8; // 	-128 ... 127
+        short short1 =  -32764; //	-32.768 ... 32.767
+        float float2 = 3.718F;//+/-1,4E-45 ... +/-3,4E+38
+
+
+
+    }
 }
